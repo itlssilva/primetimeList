4f5f33f2c1a7e7083bc4e4c5c8992a82ffdc96f4
Italo Silva <patrick.oliveira@to-brasil.com>
2020-10-14 19:28:31 -0300

    menu botoes

--- src/menu.tsx
+++ src/menu.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import { Grid } from '@material-ui/core';
+import { Box, Container, Grid, Typography } from '@material-ui/core';
 
 import Sabado from './pages/sabado';
 import Domingo from './pages/domingo';
@@ -12,10 +12,13 @@ import { eventNames } from "cluster";
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
+      flexGrow: 1,
+      background: '#eeeeee',
+    },
+    botoes: {
       '& > *': {
         margin: theme.spacing(1),
       },
-      background: '#eeeeee',
     },
   }),
 );
@@ -26,14 +29,24 @@ function AppRouter() {
   return (
     <Router>
       <div className={classes.root}>
-        <Button variant="contained" href="/">Home</Button>
-        <Button variant="contained" color="primary" href="/sabado">Sábado</Button>
-        <Button variant="contained" color="primary" href="/domingo">Domigo</Button>
-        <Button variant="contained" color="secondary" href="/segunda">Segunda</Button>
-        <Route path="/" />
-        <Route path="/sabado/" component={Sabado} />
-        <Route path="/domingo/" component={Domingo} />
-        <Route path="/segunda/" component={Segunda} />
+        <Container maxWidth="md">
+          <Grid container spacing={1}>
+            <Grid item xs={12} sm={12}>
+              <Box textAlign="center">
+                <div className={classes.botoes}>
+                  <Button variant="contained" href="/">Home</Button>
+                  <Button variant="contained" color="primary" href="/sabado">Sábado</Button>
+                  <Button variant="contained" color="primary" href="/domingo">Domigo</Button>
+                  <Button variant="contained" color="secondary" href="/segunda">Segunda</Button>
+                  <Route path="/" />
+                  <Route path="/sabado/" component={Sabado} />
+                  <Route path="/domingo/" component={Domingo} />
+                  <Route path="/segunda/" component={Segunda} />
+                </div>
+              </Box>
+            </Grid>
+          </Grid>
+        </Container>
       </div>
     </Router>
   );
